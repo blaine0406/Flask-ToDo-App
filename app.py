c2463fbc76789b000e7b491ffb23f99991b65e90
Chris Powell <blaine0406@gmail.com>
2025-08-12 21:58:19 -0400

    DB Instance Created
    Created base.html and extended it out to index.html
    Connected to sqlite via SQLALCHEMY
    imported datetime
    Created the MyTask Class and setup the properties
    added context manger

--- app.py
+++ app.py
@@ -2,9 +2,25 @@
 from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # My App
 app = Flask(__name__)
+Scss(app)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db = SQLAlchemy(app)
+
+# Data Class
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
 
 
 @app.route('/')
@@ -15,4 +31,7 @@ def index():
 
 
 if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
+    
     app.run(debug=True)
