d0f237fa904e3bf055757efde40f990adb5724e1
Chris Powell <blaine0406@gmail.com>
2025-08-14 22:21:56 -0400

    New Task
    Added Jinja to the index file to populate content, creation time and 2 buttons (Delete and Update). Added if statement to added task and see current tasks. Adde redirect and request to imports.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -8,7 +8,7 @@ from datetime import datetime
 app = Flask(__name__)
 Scss(app)
 
-
+# Configure the DB
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db = SQLAlchemy(app)
 
@@ -16,20 +16,35 @@ db = SQLAlchemy(app)
 class MyTask(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(100), nullable=False)
-    complete = db.Column(db.Integer, default=0)
+    complete = db.Column(db.Boolean, default=False)
     created = db.Column(db.DateTime, default=datetime.utcnow)
     
     def __repr__(self) -> str:
         return f"Task {self.id}"
 
-
-@app.route('/')
+### Routes
+# Homepage
+@app.route('/', methods=["POST","GET"])
 def index():
-    return render_template("index.html")
+    # Add a task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR:{e}")
+            return f"ERROR:{e}"
+    # See all current tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template("index.html", tasks=tasks)
 
 
 
-
+# Runner and debugger
 if __name__ in "__main__":
     with app.app_context():
         db.create_all()
